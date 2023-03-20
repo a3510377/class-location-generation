e5c176ead1ca57be1e27073bfe5d7306227a2e79
猴貓 <a102009102009@gmail.com>
2023-03-20 21:55:49 +0800

    fix

--- web/pages/edit/index.tsx
+++ web/pages/edit/index.tsx
@@ -1,48 +1,37 @@
 import { Table, Box, TableBody, TableRow, TableCell } from '@mui/material';
-import {
-  Add as AddIcon,
-  Fullscreen as FullscreenIcon,
-} from '@mui/icons-material';
+import { Add as AddIcon } from '@mui/icons-material';
 
 export default function Home() {
   const x = 7;
   const y = 7;
-  const users: { name: string }[] = [{ name: '你好' }, { name: '我很好' }];
 
-  const data: (number | undefined)[][] = Array.from({ length: x }).map(() =>
-    Array.from({ length: y })
-  ); // Array<Array<number>(y)>(x)
-
-  data[1][1] = 1;
+  const data: (number | undefined | null)[][] = []; // Array<Array<number>(y)>(x)
 
   return (
     <Box
       sx={{ '--seating-table-x': x, '--seating-table-y': y }}
       display="flex"
+      minHeight="100%"
       flexDirection="column"
       alignItems="center"
       justifyContent="center"
-      maxWidth="1800px"
-      width={1}
-      fontSize= {20}
-      >
+    >
       <h1>座位分配</h1>
 
       <Box
         sx={{
           p: 2,
           width: '80%',
-          height: 1,
+          maxWidth: '1200px',
+          height: '100%',
           flexDirection: 'column',
           '&,>div': { display: 'flex', alignItems: 'center' },
         }}
-      fontSize=   {20}
       >
         <Box
-      fontSize= {20}
-      sx={{
+          sx={{
             width: '90%',
-            height: 50,
+            height: 40,
             border: '1px solid black',
             '&, ~div': { justifyContent: 'center' },
           }}
@@ -53,25 +42,19 @@ export default function Home() {
           <TableBody>
             {data.map((XD, XI) => (
               <TableRow key={XI}>
-                {XD.map((data, YI) => (
+                {XD.map((YD, YI) => (
                   <TableCell
                     key={YI}
-                    sx={{
-                      p: '5px 1em',
-                      width: '100px',
-                      textAlign: 'center',
-                      lineHeight: '2em',
-                      '>div': { minHeight: '2em', margin: 'auto' },
-                    }}
+                    sx={{ padding: '2em 0', textAlign: 'center' }}
                   >
-                    <Box>{`${XI + 1}-${y - YI}`}</Box>
-                    {data === -1 ? (
-                      <Box>無效位置</Box>
-                    ) : data === void 0 ? (
-                      <Box>空</Box>
+                    {YD ? (
+                      <Box>
+                        <AddIcon />
+                      </Box>
                     ) : (
-                      <Box>{users?.[data].name}</Box>
+                      <Box></Box>
                     )}
+                    {/* {XI + 1}-{YI + 1} */}
                   </TableCell>
                 ))}
               </TableRow>
