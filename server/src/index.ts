5a5e96a38336412dca2bc6f46ed5eccb6a505380
a3510377 <a102009102009@gmail.com>
2023-06-06 21:34:06 +0800

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -32,24 +32,27 @@ interface PosData {
 
 type CacheType = (PosData | undefined)[][];
 
-const save = (users: string[], cache: CacheType) => {
-  fs.writeFileSync('data.json', JSON.stringify({ users, cache }));
-};
-
 export async function main() {
   const app = express();
   const clients: Record<string, Response> = {};
+  let simpleMap: string[] = [];
   let cache: CacheType = Array.from({ length: 7 }).map(() =>
     Array.from({ length: 5 })
   );
   let users: string[] = [];
 
-  try {
-    const data = fs.readFileSync('data.json', 'utf-8');
-    const { cache: _cache, users: _users } = JSON.parse(data);
+  const save = () => {
+    fs.writeFileSync('data.json', JSON.stringify({ users, cache, simpleMap }));
+  };
 
-    cache = _cache;
-    users = _users;
+  try {
+    const {
+      cache: _cache,
+      users: _users,
+      simpleMap: _simpleMap,
+    } = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
+
+    [cache, users, simpleMap] = [_cache, _users, _simpleMap];
     // eslint-disable-next-line no-empty
   } catch {}
 
@@ -62,7 +65,7 @@ export async function main() {
     .use(bodyParser.urlencoded({ extended: true }));
 
   const send = (eventName: string, data: unknown = '') => {
-    if (eventName === 'set') save(users, cache);
+    if (eventName === 'set') save();
 
     Object.values(clients).forEach((client) => {
       client.write(`event: ${eventName}\n`);
@@ -75,35 +78,49 @@ export async function main() {
       );
     });
   };
+  const setCatch = (x: number, y: number, userID: string) => {
+    cache[x] ||= [];
+    cache[x][y] = userID ? { id: +userID, name: users[+userID - 1] } : void 0;
+
+    console.log(`${x}-${y} => ${cache[x][y]?.name || 'null'}`);
+
+    send('set', cache);
+  };
 
   bot
     .on(Events.ClientReady, () => console.log(`Logged in as ${bot.user?.tag}`))
     .on(Events.MessageCreate, ({ channelId, content }) => {
       if (channelId !== watchChannelID) return;
 
-      if (content.startsWith('set_user')) {
+      if (content.startsWith('sim_pos')) {
+        const [, data] = content.split('\n');
+
+        simpleMap = data.split(',');
+        save();
+      } else if (content.startsWith('set_user')) {
         const data = content.split('\n');
         data.shift();
 
         users = data;
         for (const XData of cache) {
           for (const YData of XData) {
-            if (YData) YData.name = users[YData.id - 1];
+            if (!YData) return;
+
+            YData.name = users[YData.id - 1];
           }
         }
         send('set', cache);
+      } else if (/(\d+)(:(\d+))?/.test(content)) {
+        [...content.matchAll(/(\d+)(:(\d+))?/gm)].forEach(
+          ([, _id, , userID]) => {
+            const [x, y] = simpleMap[+_id - 1].split('-');
+
+            setCatch(+x - 1, +y - 1, userID);
+          }
+        );
       } else if (/(\d+)-(\d+)(:(\d+))?/.test(content)) {
         [...content.matchAll(/(\d+)-(\d+)(:(\d+))?/gm)].forEach(
-          ([, _x, _y, , userID]) => {
-            const [x, y] = [+_x - 1, +_y - 1];
-
-            cache[x] ||= [];
-            cache[x][y] = userID
-              ? { id: +userID, name: users[+userID - 1] }
-              : void 0;
-
-            send('set', cache);
-          }
+          ([, x, y, , userID]) => setCatch(+x - 1, +y - 1, userID)
         );
       }
     });
