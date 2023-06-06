6cf8c2a633ed78f82c68404afd2fb117a60974e9
a3510377 <a102009102009@gmail.com>
2023-06-06 22:34:16 +0800

    feat script

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/class-location-generation',
   plugins: [react()],
   server: {
     port: 3000,
